81e96c2b0a9c54fd34d83b5e573fdf3d4a6c3174
devin0xffffff <devin0xffffff@gmail.com>
2017-12-11 15:29:31 -0600

    Implemented main menu ui

--- Comp3490Project/Assets/_Project/Scripts/SimCamera.cs
+++ Comp3490Project/Assets/_Project/Scripts/SimCamera.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using SuperSystems.UnityTools;
 using UnityEngine;
+using UnityStandardAssets.Utility;
 
-public class SimCamera : MonoBehaviour {
+namespace Comp3490Project
+{
+    public class SimCamera : MonoBehaviour
+    {
+        private NBodySimManager nBodySimManager;
+        private FreeCamera freeCamera;
+        private AutoRotate autoRotate;
+        private SmoothFollow smoothFollow;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private void Awake()
+        {
+            nBodySimManager = GetComponent<NBodySimManager>();
+            freeCamera = GetComponent<FreeCamera>();
+            autoRotate = GetComponent<AutoRotate>();
+            smoothFollow = GetComponent<SmoothFollow>();
+        }
+
+        public void SwitchToGameMode()
+        {
+            nBodySimManager.enabled = true;
+            freeCamera.enabled = true;
+            autoRotate.enabled = false;
+            smoothFollow.enabled = false;
+        }
+
+        public void SwitchToMenuMode()
+        {
+            nBodySimManager.enabled = false;
+            freeCamera.enabled = false;
+            autoRotate.enabled = true;
+            smoothFollow.enabled = true;
+        }
+    }
 }
